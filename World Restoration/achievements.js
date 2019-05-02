ddf72c865fef576c69005bb8c96eb2462c6d2679
Naruyoko <naru519372@gmail.com>
2019-05-02 10:28:31 -0400

    Put something in Achievement

--- World Restoration/achievements.js
+++ World Restoration/achievements.js
@@ -1,1 +1,46 @@
-// I'm going to add this later.
+var Achievements={};
+function Achievement(p){
+  this.name=p.name; //achievement name
+  this.desc=p.desc; //description
+  this.id=p.id; //achievement id
+  Achievements[p.id]=this; //add to the list
+  this.cond=p.cond; //achievement earn condition, put function object.
+  this.comp=p.comp; //if completed
+  
+  this.getName=function (){
+    return this.name;
+  }
+  this.getDescription=function (){
+    return this.desc;
+  }
+  this.getID=function (){
+    return this.id;
+  }
+  this.getCondition=function (){
+    return this.cond;
+  }
+  this.updateCompletion=function (){
+    this.comp=this.comp||this.cond();
+  }
+  this.getCompletion=function (){
+    return this.comp;
+  }
+}
+
+Achievements.getAchievementById=function (id){
+  return Achievements[id]||undefined;
+}
+Achievements.asArray=function (){
+  var array=[];
+  for (var id in Achievements){
+    if (!isNaN(+id)) array[+id]=Achievements[id];
+  }
+  return array;
+}
+Achievemets.getCompletions=function (){
+  var array=[];
+  for (var id in Achievements){
+    if (!isNaN(+id)) array[+id]=Achievements[id].getCompletion();
+  }
+  return array;
+}
