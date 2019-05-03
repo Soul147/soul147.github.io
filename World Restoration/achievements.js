4da7479ac5e41f1f324eea019a71864234bfb7bd
Naruyoko <naru519372@gmail.com>
2019-05-02 20:51:53 -0400

    More achievements!

--- World Restoration/achievements.js
+++ World Restoration/achievements.js
@@ -66,5 +66,13 @@ function updateAchievements(){
 }
 
 
-createAchievement("Rule 1: The first one is free. <s>No exceptions.</s>","Get 1 FR 1",function(){return game.fr1.amount.gt(0)});
-createAchievement("Period","Start a New Episode",function(){return game.newEpisode.gt(0)});
+createAchievement("Rule 1: The first one is free. <s>No exceptions.</s>","Get 1 FR 1",function(){return game.fr1.amount.gte(1)});
+createAchievement("Period","Start a New Episode",function(){return game.newEpisode.gte(1)});
+createAchievement("Quad Core","Get 1 FR 4",function(){return game.fr4.amount.gte(1)});
+createAchievement("Three-pronged Fork","Repel 1 T forks",function(){return game.forks.gte(1e12)});
+createAchievement("Protect the Cake!","Start a New Cake at Stake",function(){return game.newCakeAtStake.gte(1)});
+createAchievement("Ten-pronged Fork","Repel 1 Dc forks",function(){return game.forks.gte(1e33)});
+createAchievement("Episodeon","Start 5 K New Episodes",function(){return game.newEpisode.gte(5000)});
+createAchievement("Where You Can Present Yourself","Start a New Contest",function(){return game.newContest.gte(1)});
+createAchievement("Feel the Power...","Get empowored cakes",function(){return game.empoworedCakes.gte(1)});
+createAchievement("Twenty-pronged Fork","Repel 1e63 forks",function(){return game.forks.gte(1e63)});
