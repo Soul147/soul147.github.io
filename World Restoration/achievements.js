f617e395bf09cc7e8ef4c96163ee3df42a94fa97
Soul147 <45136555+Soul147@users.noreply.github.com>
2019-05-10 11:55:48 -0700

    added more achievements

--- World Restoration/achievements.js
+++ World Restoration/achievements.js
@@ -66,13 +66,22 @@ function updateAchievements(){
 }
 
 
-createAchievement("Rule 1: The first one is free. <s>No exceptions.</s>","Get 1 FR 1",function(){return game.fr1.amount.gte(1)});
-createAchievement("Period","Start a New Episode",function(){return game.newEpisode.gte(1)});
-createAchievement("Quad Core","Get 1 FR 4",function(){return game.fr4.amount.gte(1)});
-createAchievement("Three-pronged Fork","Repel 1 T forks",function(){return game.forks.gte(1e12)});
-createAchievement("Protect the Cake!","Start a New Cake at Stake",function(){return game.newCakeAtStake.gte(1)});
-createAchievement("Ten-pronged Fork","Repel 1 Dc forks",function(){return game.forks.gte(1e33)});
-createAchievement("Episodeon","Start 5 K New Episodes",function(){return game.newEpisode.gte(5000)});
+createAchievement("Rule #1: The first one's always free.","Get the first Fork Repellent.",function(){return game.fr1.amount.gte(1)});
+createAchievement("One of Many","Start a New Episode.",function(){return game.newEpisode.gte(1)});
+createAchievement("Quad Core","Get a fourth tier FR.",function(){return game.fr4.amount.gte(1)});
+createAchievement("Three-pronged Fork","Repel 1 trillion forks.",function(){return game.forks.gte(1e12)});
+createAchievement("The Grind Begins","Start 500 New Episodes.",function(){return game.newEpisode.gte(5e2)});
+createAchievement("Episodeon","Start 5,000 New Episodes.",function(){return game.newEpisode.gte(5e3)});
+createAchievement("I Hope You Automated This","Start 50,000 New Episodes.",function(){return game.newEpisode.gte(5e4)});
+createAchievement("You Can Stop Now","Start 500,000 New Episodes.",function(){return game.newEpisode.gte(5e5)});
+createAchievement("Pain","Start 5,000,000 New Episodes.",function(){return game.newEpisode.gte(5e6)});
+createAchievement("Protect the Cake!","Start a New Cake at Stake.",function(){return game.newCakeAtStake.gte(1)});
+createAchievement("The 9th FR is a Lie","Get an eighth tier FR.",function(){return game.fr8.amount.gte(1)});
+createAchievement("Feel the Power","Obtain an empowered cake.",function(){return game.empoweredCakes.gte(1)});
+createAchievement("Ten-pronged Fork","Repel 1 decillion forks.",function(){return game.forks.gte(1e33)});
+createAchievement("Why?","Start 69,000 New Cake At Stakes.",function(){return game.newCakeAtStake.gte(5e3)});
 createAchievement("Where You Can Present Yourself","Start a New Contest",function(){return game.newContest.gte(1)});
-createAchievement("Feel the Power...","Get empowored cakes",function(){return game.empoweredCakes.gte(1)});
-createAchievement("Twenty-pronged Fork","Repel 1e63 forks",function(){return game.forks.gte(1e63)});
+createAchievement("Actually, Quantum Mechanics Forbids This","Get a ninth tier FR.",function(){return game.fr9.amount.gte(1)});
+createAchievement("Twenty-pronged Fork","Repel 1 vigintillion forks",function(){return game.forks.gte(1e63)});
+createAchievement("It's Over 9000","Get at least 9001 empowered cakes.",function(){return game.empoweredCakes.gte(9000)});
+createAchievement("A Googol Forks","Repel 10 dutrigintillion forks",function(){return game.forks.gte(1e100)});
